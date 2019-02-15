711e0e46a7be89936d925278bfa967c5206af2c8
denebtech <techdeneb@gmail.com>
2019-02-14 20:00:21 -0500

    Add dot handle

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -2,35 +2,68 @@
 
 document.addEventListener('DOMContentLoaded', startCalculator);
 
-var numInput = 0;
+var numInputStr = "0";
+var hasDecimal = false;
+var dingSound;
 
 function startCalculator() {
     document.getElementById("button0").addEventListener("click", numButtonClicked);
     Array.from(document.getElementsByClassName("numButtons")).forEach(button => button.addEventListener("click", numButtonClicked));
     document.getElementById("buttonDel").addEventListener("click", delButtonClicked);
+    document.getElementById("buttonDot").addEventListener("click", dotButtonClicked);
+    
+    // initialize sound element
+    dingSound = document.getElementById("dingSound");
 }
 
 function numButtonClicked(evt) {
     console.log(evt.target.innerText);
-    let numStr = getString(numInput);
 
-    if (isTooLong(numStr)) return;
+    if (isTooLong(numInputStr)) {
+        playDingSound();
+        return;
+    } 
 
-    numStr += evt.target.innerText;
-    numInput = parseFloat(numStr);
-    document.getElementById("resultText").innerHTML = numInput;
-    console.log(numInput);
+    if (numInputStr === "0") {
+        numInputStr = getString(evt.target.innerText);
+    } else {
+        numInputStr += evt.target.innerText;
+    }
+
+    document.getElementById("resultText").innerHTML = numInputStr;
+    console.log(numInputStr);
 }
 
 function delButtonClicked(evt) {
     console.log(evt.target.innerText);
-    let numStr = getString(numInput).slice(0, -1);
-    if (numStr.length == 0) {
-        numInput = 0;
-    } else {
-        numInput = parseFloat(numStr);
+
+    if (numInputStr[numInputStr.length-1] === ".") {
+        hasDecimal = false;
     }
-    document.getElementById("resultText").innerHTML = numInput;
+
+    let numStr = numInputStr.slice(0, -1);
+    if (numStr.length == 0) {
+        numInputStr = "0";
+    } else {
+        numInputStr = numStr;
+    }
+    document.getElementById("resultText").innerHTML = numInputStr;
+}
+
+function dotButtonClicked(evt) {
+    console.log("dot clicked");
+    if (hasDecimal) {
+        playDingSound();
+        return;
+    }
+    hasDecimal = true;
+    numInputStr += ".";
+    document.getElementById("resultText").innerHTML = numInputStr;
+}
+
+function playDingSound() {
+    dingSound.currentTime = 0;  // stop old playing sound
+    dingSound.play();
 }
 
 function getString(num) {
@@ -40,7 +73,6 @@ function getString(num) {
 function isTooLong(str) {
     let tmpStr = str.replace(".", "");
     if (tmpStr.length == 16) {
-        document.getElementById("dingSound").play();
         return true;
     }
     return false;
